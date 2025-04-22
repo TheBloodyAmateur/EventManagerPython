e9a059c1ab19d8748175135b5e7f3d8fcfcc7955
TheBloodyAmateur <botan_kadir@outlook.com>
2025-04-23 00:06:37 +0200

    Add docstring to CsvFormatter class to clarify its purpose

--- EventManager/formatters/csv_formatter.py
+++ EventManager/formatters/csv_formatter.py
@@ -2,6 +2,9 @@ from EventManager.formatters.default_formatter import DefaultFormatter
 
 
 class CsvFormatter(DefaultFormatter):
+    """
+    CsvFormatter is a class that formats event data into CSV format.
+    """
     def format(self, metadata, *args):
         meta = ",".join(metadata.values())
         arg_values = ",".join(arg.get_value() for arg in args)
